b55c31c6f9645d1da022e8cc31d38210e685c412
Alexander Maier <alexander.hbg@hotmail.de>
2017-10-11 15:25:54 +0200

    adding add function to superposition two rainflow matrices

--- metal-fatigue/rainfl.py
+++ metal-fatigue/rainfl.py
@@ -9,3 +9,22 @@ class rfm:
         self.ybinsize = ybinsize
         # counts should be a numpy array
         self.counts = counts
+
+
+def add(*matrices):
+    """Adds two or more rainflow matrices
+
+    Args:
+        *matrices: a list or tuple of Rainflow matrices to add. Must be the same size.
+    """
+
+    # generate an rainflow matrix with zeros
+    xbinsize = matrices[0].xbinsize
+    ybinsize = matrices[0].ybinsize
+    counts = np.zeros_like(matrices[0].counts)
+    output = rfm(xbinsize, ybinsize, counts)
+
+    # summing up the matrix entries
+    for mat in matrices:
+        output.counts += mat
+    return output
